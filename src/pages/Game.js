70ad1b0bcc5468ca801c0b75c1f2af00ae2fddff
cerdarius <serdar.savas84@gmail.com>
2020-09-27 12:59:11 +0200

    styled the gamepage

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import {
+  StyledCharacter,
+  StyledGame,
+  StyledScore,
+  StyledTimer
+} from '../styled/Game';
+import { Strong } from '../styled/Misc';
 
 export default function Game() {
   return (
-    <div>
-      <h1>Game</h1>
-    </div>
+    <StyledGame>
+      <StyledScore>
+        Score:<Strong>0</Strong>
+      </StyledScore>
+      <StyledCharacter>A</StyledCharacter>
+      <StyledTimer>
+        Time: <Strong>00: 000</Strong>
+      </StyledTimer>
+    </StyledGame>
   );
 }
