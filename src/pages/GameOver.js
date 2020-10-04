6110e84f5681166b25f3895a6d9b96491301e250
cerdarius <serdar.savas84@gmail.com>
2020-10-04 19:37:15 +0200

    Fix gameover page

--- src/pages/GameOver.js
+++ src/pages/GameOver.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useScore } from '../contexts/ScoreContext';
 import { StyledLink } from '../styled/Navbar';
 
